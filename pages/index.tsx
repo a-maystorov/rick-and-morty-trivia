78e7e6dee50d60275e3c8423203a513831e7f6d6
Alkin Maystorov <alkin971@gmail.com>
2022-10-01 17:08:31 +0200

    fetch characters from api and keep track of random char in state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,50 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
+import type { NextPage, GetServerSideProps, GetStaticProps } from 'next';
 
-const Home: NextPage = () => {
+import { Character, GetCharacterResults } from '../types';
+import { useEffect, useState } from 'react';
+
+const Home: NextPage<{ characters: Character[] }> = ({ characters }) => {
+  const [randomCharacter, setRandomCharacter] = useState<Character>();
+
+  useEffect(() => {
+    setRandomCharacter(
+      characters[Math.floor(Math.random() * characters.length)]
+    );
+  }, [characters]);
+
   return (
     <>
       <Head>
-        <title>Which is the roundest pokemon?</title>
-        <meta
-          name="description"
-          content="The answer to the most important question of all time: Which is the roundest pokemon?"
-        />
+        <title>Dead Alive or Unknown?</title>
+        <meta name="description" content="Are you a real Rick and Morty fan?" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="h-screen w-screen flex flex-col justify-center items-center">
         <header className="text-2xl text-center">
-          Which Pokemon is rounder?
+          What is the status of {randomCharacter?.name}?
         </header>
         <div className="p-4" />
         <section className="flex justify-around border rounded p-8 min-w-[50%]">
-          <div className="bg-green-300 p-8">This</div>
-          <div className="bg-red-300 p-8">or</div>
-          <div className="bg-yellow-300 p-8">This one</div>
+          <div className="bg-red-300 p-8">Dead</div>
+          <div className="bg-green-300 p-8">Alive</div>
+          <div className="bg-yellow-300 p-8">Unknown</div>
         </section>
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+  const { results }: GetCharacterResults = await res.json();
+
+  return {
+    props: {
+      characters: results,
+    },
+  };
 };
 
 export default Home;
