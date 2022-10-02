dc318ef1e755a36b255b2e021d0c634e2ab44174
Alkin Maystorov <alkin971@gmail.com>
2022-10-02 15:56:52 +0200

    get status values from buttons and add basic conditioning

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,32 @@ import Head from 'next/head';
 import type { NextPage, GetServerSideProps, GetStaticProps } from 'next';
 
 import { Character, GetCharacterResults } from '../types';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, ButtonHTMLAttributes } from 'react';
+import Image from 'next/image';
 
 const Home: NextPage<{ characters: Character[] }> = ({ characters }) => {
   const [randomCharacter, setRandomCharacter] = useState<Character>();
+  const [correct, setCorrect] = useState(false);
+  const [points, setPoints] = useState(0);
 
   useEffect(() => {
     setRandomCharacter(
       characters[Math.floor(Math.random() * characters.length)]
     );
   }, [characters]);
+
+  const handleChoice = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const choice: HTMLButtonElement = e.currentTarget;
+
+    if (choice.name === randomCharacter?.status) {
+      setCorrect(true);
+      setPoints((prev) => prev + 1);
+    }
+
+    console.log(choice.name === randomCharacter?.status);
+  };
 
   return (
     <>
@@ -26,11 +42,56 @@ const Home: NextPage<{ characters: Character[] }> = ({ characters }) => {
           What is the status of {randomCharacter?.name}?
         </header>
         <div className="p-4" />
-        <section className="flex justify-around border rounded p-8 min-w-[50%]">
-          <div className="bg-red-300 p-8">Dead</div>
-          <div className="bg-green-300 p-8">Alive</div>
-          <div className="bg-yellow-300 p-8">Unknown</div>
+        {randomCharacter && (
+          <Image
+            src={randomCharacter.image}
+            alt={randomCharacter.name}
+            width={200}
+            height={200}
+            className="rounded"
+          />
+        )}
+
+        <div className="p-4" />
+
+        {correct && (
+          <p className="text-2xl text-center pb-8">
+            {correct ? 'Correct!' : 'Wrong!'}
+          </p>
+        )}
+
+        <section className="flex justify-around border rounded p-8 min-w-[50%] text-black font-bold">
+          <button
+            className="bg-red-400 px-8 py-4 rounded cursor-pointer hover:brightness-90"
+            onClick={(e) => {
+              handleChoice(e);
+            }}
+            name="Dead">
+            Dead
+          </button>
+
+          <button
+            className="bg-green-400 px-8 py-4 rounded cursor-pointer hover:brightness-90"
+            onClick={(e) => {
+              handleChoice(e);
+            }}
+            name="Alive">
+            Alive
+          </button>
+
+          <button
+            className="bg-yellow-400 px-8 py-4 rounded cursor-pointer hover:brightness-90"
+            onClick={(e) => {
+              handleChoice(e);
+            }}
+            name="Unknown">
+            Unknown
+          </button>
         </section>
+
+        <div className="p-4" />
+
+        <footer className="text-2xl text-center">Points: {points}</footer>
       </main>
     </>
   );
